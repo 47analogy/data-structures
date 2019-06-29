e3763d97ae0fe54242f9bf89204e7fed05e2486c
analogy47 <actweed47@gmail.com>
2019-06-29 03:34:07 -0400

    add to head linked list

--- js-data-structures/linked-list.js
+++ js-data-structures/linked-list.js
@@ -14,3 +14,23 @@ console.log(linkList1);
 
 const node1 = new Node(7, 'node2', null); // head node
 console.log(node1);
+
+LinkedList.prototype.addToHead = function(value) {
+	const newNode = new Node(value, this.head, null); // this.head = null if list is empty
+
+	if (this.head) {
+		// list already has a head
+		this.head.previous = newNode; // point current head to newNode
+	} else {
+		// list is empty
+		this.tail = newNode; // newNode is also the tail
+	}
+	this.head = newNode; // newNode is now the head
+};
+
+const linkedlist2 = new LinkedList();
+linkedlist2.addToHead(4);
+linkedlist2.addToHead(5);
+linkedlist2.addToHead(6);
+
+console.log(linkedlist2);
