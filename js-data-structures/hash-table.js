feb0d4e94c11967a8c36fe9236fe5962306b36c7
analogy47 <actweed47@gmail.com>
2019-07-24 13:53:13 -0400

    add hash function to hash table

--- js-data-structures/hash-table.js
+++ js-data-structures/hash-table.js
@@ -11,5 +11,17 @@ function HashNode(key, value, next) {
 	this.next = next || null; // points to next or nothing
 }
 
+// hash function (use key to determine which bucket/index to store data)
+HashTable.prototype.hash = function(key) {
+	// key property from node
+	let total = 0;
+
+	for (let i = 0; i < key.length; i++) {
+		total += key.charCodeAt(i);
+	}
+	const bucket = total % this.numBuckets; // gets value between 0 and number of buckets
+	return bucket; //
+};
+
 const hashTable = new HashTable(20); // hash table with 20 buckets
-console.log(hashTable);
+console.log(hashTable.hash('apples'));
