e5075c2f6fec0a457d747d48908de48b7c927b84
analogy47 <actweed47@gmail.com>
2019-10-17 16:53:02 -0400

    add binary search

--- js-data-structures/search-algos/binary.search.js
+++ js-data-structures/search-algos/binary.search.js
@@ -1,22 +1,24 @@
-// returns index of search value
+// returns index of search value in a sorted list
 
-function binarySearch(arr, val) {
- let left = 0;
- let right = arr.length - 1;
- let mid = Math.floor(arr.length / 2);
+function binarySearch (arr, val) {
+  let left = 0
+  let right = arr.length - 1
+  let mid = Math.floor(arr.length / 2)
 
- while (left <= right) {
-  if (arr[mid] === val) {
-   return mid;
+  // check if mid is the search value
+  while (left <= right) {
+    if (arr[mid] === val) {
+      return mid
+    }
+    // check if search value is lower or higher than the middle
+    if (val < arr[mid]) {
+      right = mid - 1 // discard right half of array
+    } else if (val > arr[mid]) {
+      left = mid + 1 // discard left half of array
+    }
+    mid = (left + right) / 2
   }
-
-  if (val < arr[mid]) {
-   right = mid - 1;
-   mid = (left + right) / 2;
-  } else if (val > arr[mid]) {
-   left = mid + 1;
-   mid = (left + right) / 2;
-  }
- }
- return -1;
+  return -1
 }
+
+console.log(binarySearch([2, 5, 9, 13, 15, 23, 28], 23))
